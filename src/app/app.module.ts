908519f8525492991ab115a2f5807b3994d03ae6
Abdel Wadoud <88406384+rasmi-aw@users.noreply.github.com>
2023-03-01 11:51:06 +0100

    2 way data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { ExpenseEntryComponent } from './expense-entry/expense-entry.component';
-import { TestComponent } from './test/test.component';
+import {AppComponent} from './app.component';
+import {ExpenseEntryComponent} from './expense-entry/expense-entry.component';
+import {TestComponent} from './test/test.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,9 +13,11 @@ import { TestComponent } from './test/test.component';
     TestComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
